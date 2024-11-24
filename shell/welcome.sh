c3903c11c974debb7abca463ff9c24a1f88f1ef1
josh.zhong66@gmail.com <joshzhong66>
2024-11-24 10:48:37 +0800

    新增clash脚本

---
+++ shell/welcome.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+current_time=$(date)
+system_load=$(uptime | awk -F'load average: ' '{print $2}' | awk -F',' '{print $1}')
+processes=$(ps -ef | wc -l)
+memory_used=$(free | awk 'NR==2 {print "%.2f\n" $3/$2 * 100}')
+swap_total=$(free | awk 'NR==3 {print $2}')
+swap_used=$(free | awk 'NR==3 {print $3}')
+if [ $swap_total -eq 0 ]; then
+    swap_used_percentage="0"
+else
+    swap_used_percentage=$(echo "scale=2; $swap_used/$swap_total*100" | bc)
+fi
+
+disk_usage=$(df -h / | awk 'NR==2 {print $5}')
+local_address=$(hostname -I | awk '{print $1}')
+export_address=$(curl -s ifconfig.me)
+users_online=$(who | wc -l)
+
+echo ""
+echo "System load:          $system_load"
+echo "Processes:            $processes"
+echo "Memory used:          $memory_used"
+echo "Swap used:            $swap_used_percenage"
+echo "Usage On:             $disk_usage"
+echo "Local address:        $local_address"
+echo "Export address:       $export_address"
+echo "Users Online:         $users_online"
