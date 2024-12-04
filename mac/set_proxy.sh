defab9f00f9f685e67f7dc5218091df5be02547e
josh.zhong66@gmail.com <joshzhong66>
2024-12-04 22:42:37 +0800

    更新硅谷钢铁侠读书笔记

--- mac/set_proxy.sh
+++ mac/set_proxy.sh
@@ -2,15 +2,11 @@
 
 # 设置网络服务名称（你可以通过 'networksetup -listallnetworkservices' 查看你的网络服务）
 NETWORK_SERVICE="Wi-Fi"
-
-# 代理服务器地址和端口
 PROXY_SERVER="10.22.51.64"
 PROXY_PORT="7890"
-
-# 操作类型：开启代理、关闭代理或配置代理
 ACTION=$1
 
-# 函数：开启代理
+
 enable_proxy() {
     echo "开启代理..."
     networksetup -setwebproxy "$NETWORK_SERVICE" "$PROXY_SERVER" "$PROXY_PORT"
@@ -18,7 +14,6 @@ enable_proxy() {
     echo "代理已开启"
 }
 
-# 函数：关闭代理
 disable_proxy() {
     echo "关闭代理..."
     networksetup -setwebproxystate "$NETWORK_SERVICE" off
@@ -26,7 +21,6 @@ disable_proxy() {
     echo "代理已关闭"
 }
 
-# 函数：配置代理
 configure_proxy() {
     echo "配置代理..."
     networksetup -setwebproxy "$NETWORK_SERVICE" "$PROXY_SERVER" "$PROXY_PORT"
@@ -34,7 +28,6 @@ configure_proxy() {
     echo "代理已配置：$PROXY_SERVER:$PROXY_PORT"
 }
 
-# 根据传入的参数执行不同操作
 case "$ACTION" in
     enable)
         enable_proxy
